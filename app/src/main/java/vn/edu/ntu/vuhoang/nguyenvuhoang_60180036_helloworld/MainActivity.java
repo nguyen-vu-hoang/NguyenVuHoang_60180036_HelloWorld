d4260b1b7d2cd7b4c18e6eb8222649cc4f493308
Hoang <hoang.nv.60bcntt@ntu.edu.vn>
2020-04-10 14:58:17 +0700

    v1.0

---
+++ app/src/main/java/vn/edu/ntu/vuhoang/nguyenvuhoang_60180036_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.vuhoang.nguyenvuhoang_60180036_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
